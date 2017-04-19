fc4740c66b95b020b8d07db4bfb37c53d82eca50
klpk011 <sophieQ011@gmail.com>
2017-04-19 21:18:06 +0800

    implment wage part

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -25,7 +25,7 @@ class JobsController < ApplicationController
       @job = Job.find(params[:id])
     end
 
-    def updated
+    def update
       @job = Job.find(params[:id])
       if @job.update(job_params)
         redirect_to job_path
@@ -44,7 +44,7 @@ class JobsController < ApplicationController
   private
 
   def job_params
-    params.require(:job).permit(:title, :description)
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
 end
 
 end
